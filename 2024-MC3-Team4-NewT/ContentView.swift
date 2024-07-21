bfe215dad250a7dfedd6da91859a4aab2a814a0b
piri <pyoram25@gmail.com>
2024-07-21 18:11:19 +0900

    Initial Commit

---
+++ 2024-MC3-Team4-NewT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024-MC3-Team4-NewT
+//
+//  Created by ram on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024-MC3-Team4-NewT/_024_MC3_Team4_NewTApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_MC3_Team4_NewTApp.swift
+//  2024-MC3-Team4-NewT
+//
+//  Created by ram on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct _024_MC3_Team4_NewTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
