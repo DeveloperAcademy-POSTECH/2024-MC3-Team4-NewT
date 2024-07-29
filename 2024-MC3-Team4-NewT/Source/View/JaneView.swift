f72c15960e0ede72d98fedad8161ac380e04f069
darongzzang <101727090+darongzzang@users.noreply.github.com>
2024-07-29 14:47:59 +0900

    [Feat] #3 - 메인 차트 데이터만 띄우기 시도. App파일에 ModelContainer 추가

--- 2024-MC3-Team4-NewT/Source/View/JaneView.swift
+++ 2024-MC3-Team4-NewT/Source/View/JaneView.swift
@@ -6,13 +6,47 @@
 //
 
 import SwiftUI
+import SwiftData
+
 
 struct JaneView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query var chartRows: [ChartRow]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let _: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH시"
+            return formatter
+        }()
+        
+        List {
+            ForEach(chartRows, id: \.day) { row in
+                HStack {
+                    Text("\(row.day), formatter:dateFormatter")
+                    HStack(alignment: .center, spacing: 8){
+                        Image(systemName: "location.north")
+                        Text("\(row.surfingValues.windSpeed)m/s")
+                    }
+                    HStack(alignment: .center, spacing: 8) {
+                        Image(systemName: "location.north.fill")
+                        VStack {
+                            Text("\(row.surfingValues.waveHeight)m")
+                            Text("\(row.surfingValues.wavePeriod)s")
+                        }
+                    }
+                    Text("\(row.surfingValues.waterTemperature)°C")
+                    HStack(alignment: .center, spacing: 8) {
+                        Text(row.surfingValues.weather)
+                        Text("\(row.surfingValues.airTemperature)")
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    JaneView()
+extension JaneView {
+    func weatherIcon() {
+        
+    }
 }
