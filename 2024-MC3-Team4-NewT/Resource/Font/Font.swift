8761ed03cecc71d8cd1b8f4692e7134463b7d1ba
darongzzang <101727090+darongzzang@users.noreply.github.com>
2024-07-30 12:42:34 +0900

    [Feat] #16 - 폰트, 컬러 최종 추가 완료

--- 2024-MC3-Team4-NewT/Resource/Font/Font.swift
+++ 2024-MC3-Team4-NewT/Resource/Font/Font.swift
@@ -11,25 +11,34 @@ import SwiftUI
 extension Font {
     
     // Bold
-    static let pretendardBold24: Font = .custom("Pretendard-Bold", size: 24)
-    static let pretendardBold21: Font = .custom("Pretendard-Bold", size: 21)
-    static let pretendardBold18: Font = .custom("Pretendard-Bold", size: 18)
-    static let pretendardBold14: Font = .custom("Pretendard-Bold", size: 14)
-    static let pretendardBold12: Font = .custom("Pretendard-Bold", size: 12)
-    
-    // SemiBold
-    static let pretendardSemiBold18: Font = .custom("Pretendard-SemiBold", size: 18)
-    static let pretendardSemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
-    static let pretendardSemiBold14: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let HeroBold: Font = .custom("Pretendard-Bold", size: 36)
+    static let Heading1Bold: Font = .custom("Pretendard-Bold", size: 32)
+    static let Heading2Bold: Font = .custom("Pretendard-Bold", size: 24)
+    static let Heading3Bold: Font = .custom("Pretendard-Bold", size: 21)
+    static let SubheadingBold: Font = .custom("Pretendard-Bold", size: 18)
+    static let Body1Bold: Font = .custom("Pretendard-Bold", size: 16)
+    static let Body2Bold: Font = .custom("Pretendard-Bold", size: 14)
+    static let CaptionBold: Font = .custom("Pretendard-Bold", size: 12)
     
     // Medium
-    static let pretendardMedium18: Font = .custom("Pretendard-Medium", size: 18)
-    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 16)
-    static let pretendardMedium14: Font = .custom("Pretendard-Medium", size: 14)
-    static let pretendardMedium12: Font = .custom("Pretendard-Medium", size: 12)
+    static let HeroMedium: Font = .custom("Pretendard-Medium", size: 36)
+    static let Heading1Medium: Font = .custom("Pretendard-Medium", size: 32)
+    static let Heading2Medium: Font = .custom("Pretendard-Medium", size: 24)
+    static let Heading3Medium: Font = .custom("Pretendard-Medium", size: 21)
+    static let SubheadingMedium: Font = .custom("Pretendard-Medium", size: 18)
+    static let Body1Medium: Font = .custom("Pretendard-Medium", size: 16)
+    static let Body2Medium: Font = .custom("Pretendard-Medium", size: 14)
+    static let CaptionMedium: Font = .custom("Pretendard-Medium", size: 12)
     
-    // Regular
-    static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
-    static let pretendardRegular16: Font = .custom("Pretendard-Regular", size: 16)
+    
+    // ExtraLight
+    static let HeroExtraLight: Font = .custom("Pretendard-ExtraLight", size: 36)
+    static let Heading1ExtraLight: Font = .custom("Pretendard-ExtraLight", size: 32)
+    static let Heading2ExtraLight: Font = .custom("Pretendard-ExtraLight", size: 24)
+    static let Heading3ExtraLight: Font = .custom("Pretendard-ExtraLight", size: 21)
+    static let SubheadingExtraLight: Font = .custom("Pretendard-ExtraLight", size: 18)
+    static let Body1ExtraLight: Font = .custom("Pretendard-ExtraLight", size: 16)
+    static let Body2ExtraLight: Font = .custom("Pretendard-ExtraLight", size: 14)
+    static let CaptionExtraLight: Font = .custom("Pretendard-ExtraLight", size: 12)
     
 }
