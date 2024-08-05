e20c39a64e3b06268dafca883264f3ea5d0ad122
piri <pyoram25@gmail.com>
2024-08-05 10:12:31 +0900

    Chore: RecordReadView 생성

---
+++ 2024-MC3-Team4-NewT/Source/View/RecordReadView/RecordReadView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordReadView.swift
+//  2024-MC3-Team4-NewT
+//
+//  Created by ram on 8/5/24.
+//
+
+import SwiftUI
+
+struct RecordReadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordReadView()
+}
